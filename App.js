819ec023c21e2d55fb790a6a715445620ee450da
Mariana <mariana@marrari.com.br>
2023-05-12 15:02:49 -0300

    marcar campo

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {
   hadExplosion,
   wonGame,
   showMines,
+  invertFlag
 } from './src/functions';
 
 export default class App extends Component {
@@ -51,6 +52,18 @@ export default class App extends Component {
     this.setState({board, lost, won});
   };
 
+  onSelectField = (row, column) => {
+    const board = cloneBoard(this.state.board);
+    invertFlag(board, row, column);
+    const won = wonGame(board);
+
+    if (won) {
+      Alert.alert('Parabéns', 'Você Venceu!');
+    }
+
+    this.setState({board, won});
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -59,7 +72,9 @@ export default class App extends Component {
           Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
         <View style={styles.board}>
-          <MineField board={this.state.board} onOpenField={this.onOpenField} />
+          <MineField board={this.state.board} 
+            onOpenField={this.onOpenField} 
+            onSelectField={this.onSelectField}/>
         </View>
       </View>
     );
