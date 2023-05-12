53f0d6277aa67b054bd37dcd28215412bcdfaf12
Mariana <mariana@marrari.com.br>
2023-05-12 14:57:28 -0300

    abrir campo

--- App.js
+++ App.js
@@ -1,42 +1,70 @@
-import React, { Component } from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import params from './src/params'
-import MineField from './src/components/MineField'
-import { createMinedBoard } from './src/functions'
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Alert} from 'react-native';
+import params from './src/params';
+import MineField from './src/components/MineField';
+import {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines,
+} from './src/functions';
 
 export default class App extends Component {
   constructor(props) {
-    super(props)
-    this.state = this.createState()
+    super(props);
+    this.state = this.createState();
   }
 
   minesAmount = () => {
-    const cols = params.getColumnsAmount()
-    const rows = params.getRowsAmount()
-    return Math.ceil(cols * rows * params.difficultLevel)
-  }
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultLevel);
+  };
 
-  createState= () => {
-    const cols = params.getColumnsAmount()
-    const rows = params.getRowsAmount()
+  createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
     return {
       board: createMinedBoard(rows, cols, this.minesAmount()),
+      won: false,
+      lost: false,
+    };
+  };
+
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board);
+    openField(board, row, column);
+    const lost = hadExplosion(board);
+    const won = wonGame(board);
+
+    if (lost) {
+      showMines(board);
+      Alert.alert('Perdeeeeu!', 'Que buuuurro!');
     }
-  }
+
+    if (won) {
+      Alert.alert('Parabéns', 'Você Venceu!');
+    }
+
+    this.setState({board, lost, won});
+  };
 
   render() {
-    return(
+    return (
       <View style={styles.container}>
         <Text>Iniciando o Mines!</Text>
-        <Text>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        <Text>
+          Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
         <View style={styles.board}>
-          <MineField board={this.state.board} />
+          <MineField board={this.state.board} onOpenField={this.onOpenField} />
         </View>
       </View>
-    )
+    );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -46,7 +74,5 @@ const styles = StyleSheet.create({
   board: {
     alignItems: 'center',
     backgroundColor: '#AAA',
-  }
+  },
 });
-
-
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, TouchableWithoutFeedback} from 'react-native';
 import params from '../params';
 import Mine from './Mine';
 import Flag from './Flag';
@@ -23,6 +23,7 @@ export default props => {
   }
 
   return (
+    <TouchableWithoutFeedback onPress={props.onOpen}>
     <View style={styleField}>
       {!mined && opened && nearMines > 0 ? (
         <Text style={[styles.label, {color: color}]}>{nearMines}</Text>
@@ -32,6 +33,7 @@ export default props => {
       {mined && opened ? <Mine /> : false}
       {flagged && !opened ? <Flag /> : false}
     </View>
+    </TouchableWithoutFeedback>
   );
 };
 
